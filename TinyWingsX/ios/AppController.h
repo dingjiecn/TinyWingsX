cb8e867355050711ba33d3025f7957b09d76d1a8
dingjiecn <dingjiecn@gmail.com>
2012-12-13 16:45:16 +0800

    add box2d project

---
+++ TinyWingsX/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TinyWingsXAppController.h
+//  TinyWingsX
+//
+//  Created by jie ding on 12/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TinyWingsX/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TinyWingsXAppController.h
+//  TinyWingsX
+//
+//  Created by jie ding on 12/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
